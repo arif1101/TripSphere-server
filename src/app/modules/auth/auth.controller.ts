28d1a28e5cff0c1a9490fabc2a3a3baa2b51b88c
arif1101 <arifurrahmanarif223@gmail.com>
2025-07-17 11:58:16 +0600

    google authentication with passport

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -7,6 +7,8 @@ import { authServices } from "./auth.service"
 import AppError from "../../errorHelpers/AppError"
 import { setAuthCookie } from "../utils/setCookie"
 import { JwtPayload } from "jsonwebtoken"
+import { createUserTokens } from "../utils/userTokens"
+import { envVars } from "../../config/env"
 
 
 const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
@@ -80,9 +82,33 @@ const resetPassword = catchAsync(async (req: Request, res: Response, next: NextF
     })
 })
 
+const googleCallbackController = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+    let redirectTo = req.query.state ? req.query.state as string: ""
+
+    if(redirectTo.startsWith("/")){
+        redirectTo = redirectTo.slice(1)
+    }
+
+    const user = req.user;
+
+    // console.log("user", user)
+    
+    if(!user){
+        throw new AppError(httpStatus.NOT_FOUND, "User not found")
+    }
+    const tokenInfo = await createUserTokens(user)
+
+    setAuthCookie(res,tokenInfo)
+    
+    res.redirect(`${envVars.FRONTEND_URL}/${redirectTo}`)
+})
+
+
 export const AuthControllers = {
     credentialsLogin,
     getNewAccessToken,
     logout,
-    resetPassword
+    resetPassword,
+    googleCallbackController
 }
