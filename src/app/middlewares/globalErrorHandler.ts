679c13a0433cb00daa2e3a10a433d626b87ce6a4
arif1101 <arifurrahmanarif223@gmail.com>
2025-07-18 20:10:33 +0600

    Cast,validation,duplicate,Zod error handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,13 +3,43 @@
 import { NextFunction, Request, Response } from "express"
 import { envVars } from "../config/env"
 import AppError from "../errorHelpers/AppError"
+import { handlerDuplicateError } from "../helpers/handleDuplicateError"
+import { handlerValidationError } from "../helpers/handlerValidationError"
+import { TErrorSources } from "../interfaces/error.types"
+import { handleCastError } from "../helpers/handleCastError"
+import { handlerZodError } from "../helpers/handlerZodError"
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
     let statusCode = 500
     let message = "Something Went Wrong!!"
+    let errorSources : TErrorSources[] =[]
 
-    if (err instanceof AppError) {
+
+    // duplicate error handle 
+    if(err.code === 11000){
+        const simplifiedError = handlerDuplicateError(err)
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message
+    }
+    else if(err.name === "ValidationError"){
+        const simplifiedError = handlerValidationError(err)
+        statusCode = simplifiedError.statusCode;
+        errorSources = simplifiedError.errorSources as TErrorSources[]
+        message = simplifiedError.message
+    }
+    else if(err.name === "CastError"){
+        const simplifiedError = handleCastError(err)
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message
+    }
+    else if (err.name === "ZodError") {
+        const simplifiedError = handlerZodError(err)
+        statusCode = simplifiedError.statusCode
+        message = simplifiedError.message
+        errorSources = simplifiedError.errorSources as TErrorSources[]
+    }
+    else if(err instanceof AppError) {
         statusCode = err.statusCode
         message = err.message
     } else if (err instanceof Error) {
