65c2446bd25eb0be31be48b590603c968cbd5e0b
arif1101 <arifurrahmanarif223@gmail.com>
2025-07-26 20:12:54 +0600

    SSLCommerz payment systme

---
+++ src/app/modules/booking/booking.controller.ts
@@ -0,0 +1,23 @@
+import {Request, Response } from "express";
+import { catchAsync } from "../utils/catchAsync";
+import { JwtPayload } from "jsonwebtoken";
+import { sendResponse } from "../utils/sendResponse";
+import { BookingService } from "./booking.service";
+
+
+
+const createBooking = catchAsync(async(req: Request, res: Response)=> {
+    const decodeToken = req.user as JwtPayload
+    const booking = await BookingService.createBooking(req.body, decodeToken.userId)
+
+    sendResponse(res,{
+        statusCode: 201,
+        success: true,
+        message: "Booking retrieved successfully",
+        data: booking
+    })
+})
+
+export const BookingController = {
+    createBooking
+}
