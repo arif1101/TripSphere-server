412557689af246a18478345b06d40e5eb784a43f
arif1101 <arifurrahmanarif223@gmail.com>
2025-08-06 18:35:35 +0600

    set otp for verifies

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -43,6 +43,10 @@ interface EnvConfig{
         SMTP_HOST: string;
         SMTP_FROM: string;
     };
+    REDIS_HOST: string;
+    REDIS_PORT: string;
+    REDIS_USERNAME: string;
+    REDIS_PASSWORD: string;
 }
 
 const loadEnvVariables = () : EnvConfig => {
@@ -62,7 +66,10 @@ const loadEnvVariables = () : EnvConfig => {
         "SMTP_PORT",
         "SMTP_HOST",
         "SMTP_USER",
-        "SMTP_FROM"
+        "SMTP_FROM",
+        "REDIS_PORT",
+        "REDIS_USERNAME",
+        "REDIS_PASSWORD"
     ]
     requiredEnvVariables.forEach(key => {
         if(!process.env[key]){
@@ -109,6 +116,10 @@ const loadEnvVariables = () : EnvConfig => {
             SMTP_HOST: process.env.SMTP_HOST as string,
             SMTP_FROM: process.env.SMTP_FROM as string,
         },
+        REDIS_HOST: process.env.REDIS_HOST as string,
+        REDIS_PORT: process.env.REDIS_PORT as string,
+        REDIS_USERNAME: process.env.REDIS_USERNAME as string,
+        REDIS_PASSWORD: process.env.REDIS_PASSWORD as string,
         
     }
 }
