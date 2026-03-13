4633b7aef5d830683ac1ebcb87e44e9ed6249338
arif <arifurrahmanarif223@gmail.com>
2026-03-13 09:00:43 +0600

    Delete .vercel directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,14 @@
-// @ts-check
-
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
-
-export default tseslint.config(
-  eslint.configs.recommended,
-//   tseslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
-  {
-    rules: {
-    }
-  }
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+export default tseslint.config(
+  eslint.configs.recommended,
+//   tseslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  {
+    rules: {
+    }
+  }
 );
